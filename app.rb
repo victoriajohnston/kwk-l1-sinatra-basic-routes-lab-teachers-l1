10629b43e07859ebce38cf12683f98196eaf3e49
Victoria Johnston <victoria13johnston@gmail.com>
2018-08-14 15:12:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,8 +3,19 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/' do
   "Hello, World!"
-end
+  end
 
   get '/name' do
     "My name is Victoria"
-end
+  end
+  
+  get '/hometown' do
+    "My hometown is Jersey City"
+  end
+  
+  get '/favorite-song' do
+    "I don't have a favorite song"
+    
+  end 
+  
+end 
