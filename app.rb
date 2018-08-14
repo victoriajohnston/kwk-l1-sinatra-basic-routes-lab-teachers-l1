a37354b50365d7e7fb5ffc83bd9e020981822261
Victoria Johnston <victoria13johnston@gmail.com>
2018-08-14 15:12:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Victoria"
+  end
+  
+  get '/hometown' do
+    "My hometown is Jersey City"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is I don't have a favorite song"
+    
+  end 
+  
+end 
